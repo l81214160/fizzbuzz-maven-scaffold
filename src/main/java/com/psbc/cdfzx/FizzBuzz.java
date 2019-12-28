0aaf98c519a6a5c4f633a28b9ab148597133f536
l81214160 <81214160@qq.com>
2019-12-28 11:32:57 +0800

    refactor

--- src/main/java/com/psbc/cdfzx/FizzBuzz.java
+++ src/main/java/com/psbc/cdfzx/FizzBuzz.java
@@ -2,16 +2,22 @@ package com.psbc.cdfzx;
 
 public class FizzBuzz {
 
+	public static void main(String[] args) {
+		for (int i = 0; i < 101; i++) {
+			System.out.println(of(i));
+		}
+	}
+	
 	public static String of(int number) {
 		if (number % 3 == 0) {
 			if(number % 5 == 0) {
-				return "FizzBuss";
+				return "FizzBuzz";
 			}
 			return "Fizz";
 		}
 		
 		if(number % 5 == 0) {
-			return "Buss";
+			return "Buzz";
 		}
 		return String.format("%d", number);
 	}
